ed787336307d357d18ccaa3a5d3a000ac46b4a32
JM <rqt87000@gmail.com>
2022-08-20 19:20:48 +0200

    Add error message for password inputs when confirmation is different

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function pass() {
         if (password.length > 0 && confirm_pw.length > 0) {
             button.removeAttribute('disabled');
         }
+        unset_error_msg();
     } else if (password !== confirm_pw && button.hasAttribute('disabled') == false) {
-        button.setAttribute('disabled', '');
+        button.setAttribute('disabled', '');       
+    } else if (password !== confirm_pw) {
+        set_error_msg();
     }
 };
+
+function set_error_msg() {
+    let div_msg = document.getElementById('error_msg');
+    if (div_msg.textContent == '') {
+        let error_msg = document.createTextNode('* The passwords do not match');
+        div_msg.appendChild(error_msg);
+    } 
+}
+
+
+function unset_error_msg() {
+    let div_msg = document.getElementById('error_msg');
+    div_msg.textContent = '';
+}
